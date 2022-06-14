a0bc803f1614e366bd7e8ed517cf67329f436a73
EdgarMTdev <edgarmontiel961@gmail.com>
2022-06-14 17:29:02 -0500

    fix: bugs in git

---
+++ src/components/Details/ActionsButtons.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { AiOutlineShoppingCart } from "react-icons/ai"
+import { Link } from 'react-router-dom'
+import { postProducts } from '../../api'
+
+export default function ActionsButtons() {
+
+     const [productsCart, setProductsCart ] = useState([])
+
+     const addToCar = () => {
+          postProducts("/carts?populate=*")
+          .then(data => {
+               console.log(data)
+               setProductsCart(data.data.products)
+          })
+     }
+
+     return (
+          <section className=" flex gap-5">
+               <Link onClick={addToCar} to={"/shopcar"} className=" bg-secondary_green-500 w-3/4 h-12 rounded-sm flex justify-center items-center hover:opacity-90 cursor-pointer">
+                         <p className=" text-white font-semibold text-lg">GET NOW</p>
+               </Link>
+               <Link onClick={addToCar} to={"/shopcar"} className=" bg-secondary_green-300 w-1/4 h-12 rounded-sm flex justify-center items-center hover:opacity-90 cursor-pointer">
+                    <AiOutlineShoppingCart className=" w-6 h-6 text-primary-400" />
+               </Link>
+          </section>
+     )
+}
